d2c89a3fef345a14b75365429d9a7e967ced772e
Carlos Severo <severo.e.carlos@gmail.com>
2021-01-08 17:17:19 -0300

    finalized page home, with styles and responsive

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,11 +1,31 @@
 import React from "react";
-import NavBar from "../../assets/components/Navbar";
+import { Link } from "react-router-dom";
+
+import './index.css';
+import homeImage from '../../assets/images/vector/home-image.svg';
 
 export default function Home() {
   return (
-    <div>
-      <NavBar />
-      <h1>estou na home</h1>
+    <div className='home-container'>
+      <div className='home-content'>
+        <div className='home-actions'>
+          <h1 className='home-title'>
+            Faça seu pedido <br />
+            que entregamos <br />
+            pra você!!!
+          </h1>
+          <h3 className='home-subtitle'>
+            Escolha o seu pedido e em poucos minutos
+            <br /> levaremos na sua porta
+          </h3>
+          <Link to='/' className='home-btn-order'>
+            FAZER PEDIDO
+          </Link>
+        </div>
+        <div className="home-image">
+            <img src={homeImage} alt="home image"/>
+        </div>
+      </div>
     </div>
   );
 }
--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import {Switch, Route, BrowserRouter} from 'react-router-dom'
-import Home from './pages/Home';
+import React from "react";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+import NavBar from "./assets/components/Navbar";
+import Home from "./pages/Home";
 
 export default function Routes() {
-   return (
-      <BrowserRouter>
-         <Switch>
-            <Route path="/" component={Home} />
-         </Switch>
-      </BrowserRouter>
-   )
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Switch>
+        <Route path='/' component={Home} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
